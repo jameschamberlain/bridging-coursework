46c4c5c4ff578885552f6300a1b7b38600f6f7e9
jameschamberlain <james99chamberlain1999@gmail.com>
2020-08-26 12:11:20 +0100

    Updated design and added landing page

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'),)
-]
+    path('', include('landing.urls')),
+    path('blog/', include('blog.urls')),
+    ]
